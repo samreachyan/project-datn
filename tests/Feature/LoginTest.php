9f1954d5ba09bd4d7fd16e066c82a6a18539b0e1
samreachyan <samreachyan@gmail.com>
2021-03-24 17:42:02 +0700

    test

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testUserLoginSuccessfully()
+    {
+        $payload = [
+            'email' => "sabayblc@gmail.com",
+            'password' => bcrypt('sabayblc')
+        ];
+
+        $this->json('POST', 'api/login', $payload)
+            ->assertStatus(200);
+    }
+}
